2cc0093a6c71b337b870c74f6bf8e7c30a92254a
natto <natto@natto-tpy.none>
2018-03-06 23:38:43 -0800

    init runnable with readme

---
+++ src/example/main.cljs
@@ -0,0 +1,39 @@
+(ns example.main
+  (:require ["electron" :as e :refer (app BrowserWindow)]
+            ["path" :as path]
+            ["url" :as url]))
+
+(defonce win-ref (atom nil))
+
+(defn create-window []
+  (let [win
+        (BrowserWindow. #js {:width 800
+                             :height 600})
+
+        url
+        (url/format #js {:pathname (path/join js/__dirname "index.html")
+                         :protocol "file:"
+                         :slashes true})]
+
+    (.loadURL win url)
+
+    (.. win -webContents (openDevTools))
+
+    (reset! win-ref win)
+
+    (.on win "closed"
+      (fn [e]
+        (reset! win-ref nil)))))
+
+(defn maybe-quit []
+  (when (not= js/process.platform "darwin")
+    (.quit app)))
+
+(defn maybe-create-window []
+  (when-not @win-ref
+    (create-window)))
+
+(defn main []
+  (.on app "ready" create-window)
+  (.on app "activate" maybe-create-window)
+  (.on app "window-all-closed" maybe-quit))
